cac5648403fa167f3ea248b1dcd3c582b2afd2ab
Keolos <abdullahmohammednaji@gmail.com>
2025-07-13 00:09:39 +0000

    update

--- 9-add.js
+++ 9-add.js
@@ -1,8 +1,8 @@
-function add(a, b, c) {
-  return a + b + c;
+function add(a, b) {
+  return a + b;
 }
 
 const a = parseInt(process.argv[2]);
 const b = parseInt(process.argv[3]);
-const c = parseInt(process.argv[4]);
-console.log(add(a, b, c));
+
+console.log(add(a, b));
